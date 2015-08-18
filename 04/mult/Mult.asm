033998887a82cbd0d7ac06d46b120290f5765cab
federico.rebora <federico.rebora@kevurugames.com>
2015-08-18 19:55:55 +0100

    Unsigned multiplication implemented in ASM

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,42 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// a=R0, b=R1, result=R2
+
+// result=0
+@R2
+M = 0
+
+// i=b
+@R1
+D = M
+
+@i
+M = D
+
+(LOOP)
+// if D==0 JMP to END
+@i
+D=M
+
+@END
+D; JEQ
+
+// D=a
+@R0
+D=M
+
+// result += a
+@R2
+M=M+D
+
+// i--
+@i
+M=M-1
+
+@LOOP
+0; JMP
+
+(END)
+@END
+0; JMP
