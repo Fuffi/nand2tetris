01dda05ddec9d6cb1e8f5a10024917f75760b799
federico.rebora <federico.rebora@kevurugames.com>
2015-08-18 20:28:23 +0100

    Screen fill on kbd input implemented in ASM

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -3,9 +3,59 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+(LOOP)
+@8191
+D = A
+
+@i
+M = D
+
+@KBD
+D = M
+
+@BLACK
+D; JNE
+
+(WHITE)
+@i
+D = M
+
+@LOOP
+D; JLT
+
+@SCREEN
+A = A + D
+M = 0
+
+@i
+M = M - 1
+
+@WHITE
+0; JMP
+
+(BLACK)
+@i
+D = M
+
+@LOOP
+D; JLT
+
+@SCREEN
+A = A + D
+M = -1
+
+@i
+M = M - 1
+
+@BLACK
+0; JMP
+
+@LOOP
+0; JMP
